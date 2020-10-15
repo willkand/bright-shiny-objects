2a137b5f819dbe7b063792f8e37d875e79748ad4
Colin Jaffe <balloonasaurus@gmail.com>
2020-10-14 21:18:01 -0400

    Organizes and explains the different types of tasks more clearly.

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const myCodeImmersivesObj = {
   objectify: 'me'
 };
 
-// Set the value of property `objectify` to be "yourself".
+// Part 1 - Adding and changing properties in plain JS.
+// Here, we'll be changing and adding properties BELOW the creation of the object.
+// You won't change lines 1-3 above, but instead set and change the properties for each step below by writing NEW code underneath each comment.
+
+// Below, set the value of property `objectify` to be "yourself".
 
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
@@ -26,7 +30,8 @@ const myCodeImmersivesObj = {
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
 
 
-// Now make a new EMPTY object that represents yourself.
+// Part 2 - your own object.
+// Now make a new EMPTY object that represents yourself. You won't put any properties in the curly braces, but just like in part 1, add them in each step below using plain JavaScript statements.
 
 
 // Add a `firstName` property and set it to a string.
@@ -47,6 +52,8 @@ const myCodeImmersivesObj = {
 // Now push three strings into that array.
 
 
+// Part 3 - setting key-value pairs in-line.
 // Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `codeImmersivesObject` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of numbers. The important thing here is: this should all be done within the brackets of the object, not below the place where the object is declared.
 
-// As a stretch goal, pre-populate the object with at least one property using bracket notation as well. 
+
+// As a stretch goal, go back pre-populate the object with at least one property using a key name that CAN'T be a variable name. That would be anything with a space in it, or a dash, or starting with a number.
